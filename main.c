58cf07b4ee71233f4aaeed78a4c34bca682aefb5
ChaeyunK <iehate52@gmail.com>
2023-11-19 22:07:18 +0900

    ex01

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+int i=10;
+char c='a';
+
+   
+    int* iptr = &i;
+    char* cptr = &c;
+    int* iptr2 = iptr;
+
+    printf("i: %d, iptr: %p\n", i, (void*)iptr);
+    printf("c: %c, cptr: %p\n", c, (void*)cptr);
+    printf("iptr2: %p, value: %d\n", (void*)iptr2, *iptr2);
+
+    return 0;
+}
